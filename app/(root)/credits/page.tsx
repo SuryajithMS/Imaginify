d0dfa4d5a705c26afa7fbecee76ff921495657a6
Your Name <suryajithmelila075@gmail.com>
2024-04-03 22:52:05 +0530

    intial commit

---
+++ app/(root)/credits/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Creditpage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Creditpage
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTransformationpage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddTransformationpage
